aaa76a7802cf149691b142c8b06defce9f8e2a11
Hans Bragman <hans.bragman@soprasteria.com>
2020-11-03 12:09:15 +0100

    Added mostRecentContent to the query object FindConteentType.

--- GetAggregatedRequestExemptionStatuses-v1-main/src/main/java/se/skltp/aggregatingservices/riv/financial/patientfees/exemption/requestexemptionstatuses/GARESAgpServiceFactoryImpl.java
+++ GetAggregatedRequestExemptionStatuses-v1-main/src/main/java/se/skltp/aggregatingservices/riv/financial/patientfees/exemption/requestexemptionstatuses/GARESAgpServiceFactoryImpl.java
@@ -1,10 +1,15 @@
 package se.skltp.aggregatingservices.riv.financial.patientfees.exemption.requestexemptionstatuses;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
+import org.apache.cxf.message.MessageContentsList;
 import riv.financial.patientfees.exemption.requestexemptionstatusesresponder.v1.RequestExemptionStatusesResponseType;
 import riv.financial.patientfees.exemption.requestexemptionstatusesresponder.v1.RequestExemptionStatusesType;
 import se.skltp.aggregatingservices.AgServiceFactoryBase;
+import se.skltp.aggregatingservices.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentType;
 
 @Log4j2
 public class GARESAgpServiceFactoryImpl extends
@@ -13,6 +18,13 @@ public class GARESAgpServiceFactoryImpl extends
   @Override
   public String getPatientId(RequestExemptionStatusesType queryObject){
     return queryObject.getPatientId().getExtension();
+  }
+
+  @Override
+  public FindContentType createFindContent(MessageContentsList messageContentsList) {
+    FindContentType findContentType = super.createFindContent(messageContentsList);
+    findContentType.setMostRecentContent(getDateStringOneYearBack());
+    return findContentType;
   }
 
   @Override
@@ -25,6 +37,17 @@ public class GARESAgpServiceFactoryImpl extends
 
     RequestExemptionStatusesResponseType aggregatedResponse=new RequestExemptionStatusesResponseType();
     return aggregatedResponse;
+  }
+
+  private String getDateStringOneYearBack() {
+    SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+    Date now = new Date();
+    Calendar c = Calendar.getInstance();
+    c.setTime(now);
+    c.add(Calendar.YEAR, -1);
+    Date then = c.getTime();
+    String ret = format.format(then);
+    return ret;
   }
 }
 
