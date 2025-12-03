43188d57aea2f2c40a7b2b73d02aa7a57f34a68b
Jonas Mattsson <jonas.mattsson@nordicmedtest.se>
2025-12-03 09:16:01 +0100

    override createRequestList to force the list to be created from logicalAddress instead of sourceSystem as the super class default suggests

--- GetAggregatedRequestExemptionStatuses-v1-main/src/main/java/se/skltp/aggregatingservices/riv/financial/patientfees/exemption/requestexemptionstatuses/GARESAgpServiceFactoryImpl.java
+++ GetAggregatedRequestExemptionStatuses-v1-main/src/main/java/se/skltp/aggregatingservices/riv/financial/patientfees/exemption/requestexemptionstatuses/GARESAgpServiceFactoryImpl.java
@@ -1,15 +1,18 @@
 package se.skltp.aggregatingservices.riv.financial.patientfees.exemption.requestexemptionstatuses;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+
 import lombok.extern.log4j.Log4j2;
 import org.apache.cxf.message.MessageContentsList;
 import riv.financial.patientfees.exemption.requestexemptionstatusesresponder.v1.RequestExemptionStatusesResponseType;
 import riv.financial.patientfees.exemption.requestexemptionstatusesresponder.v1.RequestExemptionStatusesType;
 import se.skltp.aggregatingservices.AgServiceFactoryBase;
+import se.skltp.aggregatingservices.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentResponseType;
 import se.skltp.aggregatingservices.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentType;
+import se.skltp.aggregatingservices.riv.itintegration.engagementindex.v1.EngagementType;
+
+import static se.skltp.aggregatingservices.utility.RequestListUtil.createRequest;
 
 @Log4j2
 public class GARESAgpServiceFactoryImpl extends
@@ -37,6 +40,27 @@ public class GARESAgpServiceFactoryImpl extends
 
     RequestExemptionStatusesResponseType aggregatedResponse=new RequestExemptionStatusesResponseType();
     return aggregatedResponse;
+  }
+
+  @Override
+  public List<MessageContentsList> createRequestList(
+          MessageContentsList messageContentsList, FindContentResponseType eiResp) {
+       int initialCapacity = eiResp.getEngagement().size();
+      log.info("Got {} hits in the engagement index", initialCapacity);
+      Set<String> addresses = new HashSet<>(initialCapacity);
+      for (EngagementType e : eiResp.getEngagement()) {
+          String la = e.getLogicalAddress();
+          if (la != null) {
+              addresses.add(la);
+          }
+      }
+      List<MessageContentsList> reqList = new ArrayList<>(addresses.size());
+      for (String la : addresses) {
+          log.info("Calling source system using logical address {}", la);
+          reqList.add(createRequest(la, messageContentsList));
+      }
+      log.info("Calling {} source systems", reqList.size());
+      return reqList;
   }
 
   private String getDateStringOneYearBack() {
